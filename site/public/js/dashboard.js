875ac9eb4c0251403c8fb7955cb95060d6851370
maarcos01 <toretomarcos50@gmail.com>
2023-11-13 23:06:17 -0300

    criei uma function para repassar os valores do select para outra tela

--- site/public/js/dashboard.js
+++ site/public/js/dashboard.js
@@ -1,23 +1,35 @@
-const monitorEspecialidades = document.getElementById('tela_especialidades');
-const monitorDashboard = document.getElementById('tela_dashboard');
-const monitorGuia = document.getElementById('tela_guia');
+const monitorEspecialidades = document.getElementById("tela_especialidades");
+const monitorDashboard = document.getElementById("tela_dashboard");
+const monitorGuia = document.getElementById("tela_guia");
 
-function trocaDeTela(tela){ 
-    var monitores = [monitorEspecialidades, monitorDashboard, monitorGuia]
+function maisOpcoes() {
+  var menu = Number(menuSelect.value);
+  var direcionamentos = [
+    "../sobre",
+    "mail.to/marcos.floriano@sptech.school",
+    "../login",
+  ];
+  for (var i = 0; i < 4; i++) {
+    if (menu == i) window.open(direcionamentos[i], "_blank");
+  }
+}
 
-    for(var i=0; i<monitores.length; i++){
-        if(tela == 0){
-            monitores[0].style.display = "flex";
-            monitores[1].style.display = "none";
-            monitores[2].style.display = "none";
-        }else if(tela == 1){
-            monitores[0].style.display = "none";
-            monitores[1].style.display = "flex";
-            monitores[2].style.display = "none";
-        }else{
-            monitores[0].style.display = "none";
-            monitores[1].style.display = "none";
-            monitores[2].style.display = "flex"
-        }
+function trocaDeTela(tela) {
+  var monitores = [monitorEspecialidades, monitorDashboard, monitorGuia];
+
+  for (var i = 0; i < monitores.length; i++) {
+    if (tela == 0) {
+      monitores[0].style.display = "flex";
+      monitores[1].style.display = "none";
+      monitores[2].style.display = "none";
+    } else if (tela == 1) {
+      monitores[0].style.display = "none";
+      monitores[1].style.display = "flex";
+      monitores[2].style.display = "none";
+    } else {
+      monitores[0].style.display = "none";
+      monitores[1].style.display = "none";
+      monitores[2].style.display = "flex";
     }
+  }
 }
