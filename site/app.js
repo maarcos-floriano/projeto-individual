0c3a2a019e61891a0e62c477e7957ec3250bce39
maarcos-floriano <marcos.floriano@sptech.school>
2023-11-14 15:55:02 -0300

    adicionei as rotas do guia no app

--- site/app.js
+++ site/app.js
@@ -14,6 +14,7 @@ var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var patrulhasRouter = require("./src/routes/patrulhas");
+var guiaRouter = require("./src/routes/guia")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,6 +28,7 @@ app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/patrulhas", patrulhasRouter);
+app.use("/guia", guiaRouter)
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
